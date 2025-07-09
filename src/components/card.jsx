33f9d724651c4312968eb27228157e7f8d58534e
Anas Khan <muhammadanaskhanak@gmail.com>
2025-07-10 04:25:36 +0500

    update textarea classname

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -38,7 +38,8 @@ export const Textarea = React.forwardRef(({ className = "", ...props }, ref) => 
   return (
     <textarea
       ref={ref}
-      className={`flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm ${className}`}
+      
+      className={`flex min-h-[200px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm ${className}`}
       {...props}
     />
   );
